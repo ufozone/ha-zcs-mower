8ba70cae11dfeae0f6ac7e96136d6679111d51dc
Markus <Markus@Markus-HP450G4>
2023-05-04 01:02:55 +0200

    fix services descriptions

--- custom_components/zcsmower/const.py
+++ custom_components/zcsmower/const.py
@@ -58,8 +58,8 @@ SERVICE_WORK_UNTIL_SCHEMA = vol.Schema(
     {
         vol.Required(ATTR_DEVICE_ID): cv.entity_ids_or_uuids,
         vol.Required("area"): vol.All(vol.Coerce(int), vol.Range(min=1, max=8)),
-        vol.Required("hours"): vol.All(vol.Coerce(int), vol.Range(min=1, max=24)),
-        vol.Required("minutes"): vol.All(vol.Coerce(int), vol.Range(min=1, max=60)),
+        vol.Required("hours"): vol.All(vol.Coerce(int), vol.Range(min=0, max=24)),
+        vol.Required("minutes"): vol.All(vol.Coerce(int), vol.Range(min=0, max=60)),
     }
 )
 SERVICE_BORDER_CUT = "border_cut"
@@ -72,8 +72,8 @@ SERVICE_CHARGE_UNTIL = "charge_until"
 SERVICE_CHARGE_UNTIL_SCHEMA = vol.Schema(
     {
         vol.Required(ATTR_DEVICE_ID): cv.entity_ids_or_uuids,
-        vol.Required("hours"): vol.All(vol.Coerce(int), vol.Range(min=1, max=24)),
-        vol.Required("minutes"): vol.All(vol.Coerce(int), vol.Range(min=1, max=60)),
+        vol.Required("hours"): vol.All(vol.Coerce(int), vol.Range(min=0, max=24)),
+        vol.Required("minutes"): vol.All(vol.Coerce(int), vol.Range(min=0, max=60)),
         vol.Required("weekday"): vol.All(vol.Coerce(int), vol.Range(min=0, max=6)),
     }
 )
