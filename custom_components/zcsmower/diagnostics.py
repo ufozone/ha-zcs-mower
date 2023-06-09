21e5a18b85cb1e4e051858e65b93803c8a2627c5
Markus <Markus@Markus-HP450G4>
2023-06-09 22:07:17 +0200

    add diagnostics support

---
+++ custom_components/zcsmower/diagnostics.py
@@ -0,0 +1,38 @@
+"""Diagnostics support for ZCS Lawn Mower Robot."""
+from __future__ import annotations
+
+from homeassistant.components.diagnostics import async_redact_data
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import (
+    DOMAIN,
+    CONF_CLIENT_KEY,
+    CONF_TRACE_POSITION_ENABLE,
+    CONF_CAMERA_ENABLE,
+    CONF_MAP_HISTORY_ENABLE,
+    CONF_MAP_GPS_TOP_LEFT,
+    CONF_MAP_GPS_BOTTOM_RIGHT,
+)
+
+TO_REDACT = {
+    CONF_CLIENT_KEY,
+    CONF_TRACE_POSITION_ENABLE,
+    CONF_CAMERA_ENABLE,
+    CONF_MAP_HISTORY_ENABLE,
+    CONF_MAP_GPS_TOP_LEFT,
+    CONF_MAP_GPS_BOTTOM_RIGHT,
+}
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, config_entry: ConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics of the config entry and lawn mower data."""
+    coordinator = hass.data[DOMAIN][config_entry.entry_id]
+    diagnostics_data = {
+        "config_entry_data": async_redact_data(entry.as_dict(), TO_REDACT),
+        "coordinator_data": coordinator.data,
+    }
+
+    return diagnostics_data
