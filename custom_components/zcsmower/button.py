600c6ede957864d8a380c738135cd7e3f1e8436f
Markus <Markus@Markus-HP840G10.localdomain>
2023-09-08 21:17:27 +0200

    revert disable button entities by default

--- custom_components/zcsmower/button.py
+++ custom_components/zcsmower/button.py
@@ -70,8 +70,6 @@ async def async_setup_entry(
 class ZcsMowerButtonEntity(ZcsMowerEntity, ButtonEntity):
     """Representation of a ZCS Lawn Mower Robot button."""
 
-    _attr_entity_registry_enabled_default = False
-
     def __init__(
         self,
         hass: HomeAssistant,
