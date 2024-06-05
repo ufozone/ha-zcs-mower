013cf9c2ad82791ca6888dad9864bfe98b296e2b
ufozone <dev@ufozone.de>
2024-06-05 22:52:48 +0200

    Revert changes on device_tracker entity

--- custom_components/zcsmower/device_tracker.py
+++ custom_components/zcsmower/device_tracker.py
@@ -1,10 +1,6 @@
 """ZCS Lawn Mower Robot sensor platform."""
 from __future__ import annotations
 
-import os
-
-from datetime import timedelta
-
 from homeassistant.core import HomeAssistant
 from homeassistant.const import (
     ATTR_LOCATION,
@@ -22,10 +18,7 @@ from homeassistant.helpers.entity import (
 )
 
 from .const import (
-    LOGGER,
     DOMAIN,
-    CONF_MAP_MARKER_PATH,
-    LOCATION_HISTORY_DAYS,
 )
 from .coordinator import ZcsMowerDataUpdateCoordinator
 from .entity import ZcsMowerEntity
@@ -121,14 +114,3 @@ class ZcsMowerTrackerEntity(ZcsMowerEntity, TrackerEntity):
     def device_class(self):
         """Return Device Class."""
         return None
-
-    @property
-    def entity_picture(self) -> str | None:
-        """Return the picture of the device."""
-        map_marker_path = self.config_entry.options.get(CONF_MAP_MARKER_PATH, None)
-        if not map_marker_path or not os.path.isfile(map_marker_path):
-            return None
-
-        # TODO: config attribute for using marker as entity picture
-        # "/local/l35.png"
-        return map_marker_path
